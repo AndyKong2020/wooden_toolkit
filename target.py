206305f3e9ec389cc384f1cff39c8f634ec0c15b
AndyKong2020 <andy.kong@nuaa.edu.cn>
2022-07-07 22:19:42 +0800

    init

---
+++ target.py
@@ -0,0 +1,4 @@
+# 作者:明月清风我
+# 时间:2022/7/7 17:28
+class Target:
+    def __init__(self):
---
+++ template.py
@@ -0,0 +1,6 @@
+# 作者:明月清风我
+# 时间:2022/7/7 17:36
+class template:
+    def __init__(self, name, location):
+        self.name = name
+        self.location = location
